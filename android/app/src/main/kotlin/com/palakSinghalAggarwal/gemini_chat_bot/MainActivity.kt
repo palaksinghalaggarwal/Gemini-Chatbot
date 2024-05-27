dee35b4c719c8e92f369badecafe827da76133af
palaksinghalaggarwal <gpalak044@gmail.com>
2024-05-28 01:29:59 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/palakSinghalAggarwal/gemini_chat_bot/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.palakSinghalAggarwal.gemini_chat_bot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '/firebase_options.dart';
+import '/screens/home_screen.dart';
+import '/screens/login_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.userChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.data == null) {
+            return const LoginScreen();
+          }
+
+          return const HomeScreen();
+        },
+      ),
+    );
+  }
+}
